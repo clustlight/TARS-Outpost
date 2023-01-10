585d56084720f24cf4a8a55fd74240b2113ece92
quadseed <contact@quadseed.net>
2023-01-10 13:02:34 +0900

    add webhook relay

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
+import json
+import os
+
+import dotenv
 from fastapi import FastAPI, Body, WebSocket, WebSocketDisconnect
 
 from websocket import ConnectionManager
 
+dotenv.load_dotenv()
+
 manager = ConnectionManager()
 app = FastAPI()
 
+webhook_signature = os.environ.get("WEBHOOK_SIGNATURE")
 
 @app.post("/")
 async def webhook_endpoint(body=Body(...)):
-    data = str(body, "UTF-8")
-    await manager.broadcast(data)
-    return {"message": "OK"}
+    if "signature" in body and "event" in body and "movie" in body and "broadcaster" in body:
+        if body["signature"] == webhook_signature:
+            if body["event"] == "livestart" or body["event"] == "liveend":
+                await manager.broadcast(json.dumps(body))
+                return {"message": "OK"}
 
 
 @app.websocket("/")
