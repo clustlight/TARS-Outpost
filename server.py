6135fb084fb0bb3008edfb9069b1773f90bf4831
quadseed <contact@quadseed.net>
2026-02-06 23:23:28 +0900

    Add debug message and workaround for signature mismatch

--- server.py
+++ server.py
@@ -15,11 +15,26 @@ webhook_signature = os.environ.get("WEBHOOK_SIGNATURE")
 
 @app.post("/")
 async def webhook_endpoint(body=Body(...)):
-    if "signature" in body and "event" in body and "movie" in body and "broadcaster" in body:
-        if body["signature"] == webhook_signature:
-            if body["event"] == "livestart" or body["event"] == "liveend":
-                await manager.broadcast(json.dumps(body))
-                return {"message": "OK"}
+    print(body)
+    if "signature" in body:
+        event = body.get("event")
+        if event in ('livestart', 'liveend'):
+            await manager.broadcast(json.dumps(body))
+            print(
+                f"live event: {body['event']} | Broadcaster: {body['broadcaster']['screen_id']}({body['broadcaster']['id']})"
+            )
+            print(
+                f"signature is {'MATCH' if body['signature'] == webhook_signature else 'MISMATCH'} ({body['signature']})"
+            )
+            return {"message": "OK"}
+        elif event in ('liveschedulecreate', 'livescheduleupdate', 'livescheduledelete'):
+            print(
+                f"live schedule event: {body['event']} | Broadcaster: {body['broadcaster']['screen_id']}({body['broadcaster']['id']})"
+            )
+            print(
+                f"signature is {'MATCH' if body['signature'] == webhook_signature else 'MISMATCH'} ({body['signature']})"
+            )
+            return {"message": "OK"}
 
 
 @app.websocket("/")
